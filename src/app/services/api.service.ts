05f3fae8ea3c404fcc5f8ab7d8405eee62fd18d1
Iman Ragab <iman.mansour94@gmail.com>
2018-06-26 17:18:56 +0200

    add api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  private formatErrors(error: any) {
+    return throwError(error.error);
+  }
+
+  // make api get request
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http.get(`${environment.api_host}${path}`, { params })
+      .pipe(catchError(this.formatErrors));
+  }
+
+  // make api post request 
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_host}${path}`,
+      JSON.stringify(body)
+    ).pipe(catchError(this.formatErrors));
+  }
+
+  // make api put request 
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http.put(
+      `${environment.api_host}${path}`,
+      JSON.stringify(body)
+    ).pipe(catchError(this.formatErrors));
+  }
+
+  // make api delete request
+  delete(path): Observable<any> {
+    return this.http.delete(
+      `${environment.api_host}${path}`
+    ).pipe(catchError(this.formatErrors));
+  }
+}
